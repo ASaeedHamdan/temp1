e2f53a028cb529dc861d7a7d17f05923ef2cb9d6
ASaeedHamdan <89150047+ASaeedHamdan@users.noreply.github.com>
2021-08-28 19:42:23 +0200

    fd

---
+++ js/asd.js
@@ -0,0 +1,8 @@
+  //text typing animation //
+  $('.example1').typeIt({
+    strings: 'This is a simple string.',
+    speed: 150,
+    autoStart: false,
+    breakDelay: 1500,
+    startDelay: 1000
+});
